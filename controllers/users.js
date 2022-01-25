db34f3b9403f14758192bdb3b787b61e95f36cd4
stocke777 <deshwaljaivardhan@gmail.com>
2022-01-25 22:13:07 +0530

    login function added

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 
 import { ObjectId } from "mongodb";
 import { default as mongodb } from 'mongodb';
+import {default as bcrypt} from 'bcrypt';
 
 let MongoClient = mongodb.MongoClient;
 const url = 'mongodb://127.0.0.1:27017';
@@ -14,7 +15,6 @@ MongoClient.connect(url, {
     if (err) {
         return console.log(err);
     }
-
     // Specify database you want to access
     db = client.db('testing');
 
@@ -22,26 +22,13 @@ MongoClient.connect(url, {
 });
 
 
-let users = [];
 export const getAllUsers = (req, res)=>{
     let customers = db.collection('customers');
-    customers.find().toArray((err, results) => {
+    customers
+        .find({})
+        .toArray((err, results) => {
         res.send(results);
     });
-};
-
-export const createUser = (req, res)=>{
-    const user = req.body;
-    let customers = db.collection('customers');
-    try{
-        customers.insertOne({ firstName: user.firstName, lastName: user.lastname, age: user.age  }, (err, result) => {
-            if(err) throw err;
-        });
-    }
-    catch(e){
-        res.send("Provide correct details please.")
-    }
-    res.send(`User with name ${user.firstName} saved.`);
 };
 
 export const getUser = (req, res)=>{
@@ -62,6 +49,63 @@ export const getUser = (req, res)=>{
     }
 };
 
+export const createUser = async (req, res)=>{
+    const user = req.body;
+
+    if (!user.password) res.send("Please provide password!!!");
+
+    try{
+
+        const salt = await bcrypt.genSalt();
+        const hashedpassword = await bcrypt.hash(user.password, salt);
+
+        let customers = db.collection('customers');
+        customers.insertOne({ firstName: user.firstName, lastName: user.lastname, age: user.age, password: hashedpassword }, (err, result) => {
+            if(err) throw err;
+        });
+    }
+    catch(e){
+        res.send("Provide correct details please.")
+    }
+    res.send(`User with name ${user.firstName} saved.`);
+};
+
+export const loginUser = (req, res)=>{
+    const {id} = req.params;
+
+    try{
+        let customers = db.collection('customers');
+        let user = {}
+        customers.find({_id:ObjectId(id)}).toArray((e, r)=>{
+            if(e) throw e;
+            user = r[0];
+            console.log(r[0]);
+            if(!user){
+                return res.status(400).send("Cant find User with this id");
+            }else{
+                try{
+                    bcrypt.compare(req.body.password, user.password, function(err, isMatch) {
+                        if (err) {
+                        throw err
+                        } else if (!isMatch) {
+                        console.log("Password doesn't match!")
+                        return res.send("Password not matched")
+                        } else {
+                        console.log("Password matches!")
+                        return res.send("Success Login")
+                        }
+                    })
+                }catch(e){
+                    return res.send("Login Failed")
+                }
+            }
+        });
+    }catch(e){
+        return res.send("Invalid Id");
+    }
+    
+}
+
 export const deleteUser = (req, res)=>{
     const {id} = req.params;
     try{
@@ -70,7 +114,7 @@ export const deleteUser = (req, res)=>{
         });
     }catch(e){
         res.send("Invalid Id.")
-    };
+    }
     res.send(`User with id ${id} deleted.`);
 };
 
@@ -78,7 +122,7 @@ export const patchUser = (req, res)=>{
     const {id} = req.params;
     const {firstName, lastName, age} = req.body;
     try{
-        let user = db.collection("customers").findOne({_id:ObjectId(id)}, (err, results)=>{
+        db.collection("customers").findOne({_id:ObjectId(id)}, (err, results)=>{
             if(err) throw err;
             db.collection("customers").updateOne({_id:ObjectId(id)}, {$set: {firstName: firstName, lastName:lastName, age:age}})
         });
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 
 import express from 'express';
-import {getAllUsers, createUser, getUser, deleteUser, patchUser} from '../controllers/users.js';
+import {getAllUsers, createUser, loginUser, getUser, deleteUser, patchUser} from '../controllers/users.js';
 
 const router = express.Router();
 
@@ -14,6 +14,8 @@ router.get('/:id', getUser);
 router.delete('/:id', deleteUser);
 
 router.patch('/:id', patchUser);
+
+router.post('/:id', loginUser);
 
 
 export default router;
